fbd81632ba7aeed1f047af90cd905e226d4f73d6
Semen Gricenko <semengricenko@gmail.com>
2020-01-04 11:29:27 +0500

    First version of base TCP server

---
+++ BaseTCPServer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+namespace web
+{
+	class BaseTCPServer
+	{
+	protected:
+		WSADATA wsaData;
+
+		SOCKET listenSocket;
+
+		addrinfo* info;
+		addrinfo hints;
+
+		const std::string port;
+		bool freeDLL;
+		bool isRunning;
+
+	protected:
+		virtual void receiveConnections();
+
+		virtual void clientConnection(SOCKET clientSocket) = 0;
+
+	public:
+		BaseTCPServer(const std::string& port, bool freeDLL = true);
+
+		virtual void start();
+
+		virtual void stop();
+
+		virtual bool serverState() const;
+
+		virtual ~BaseTCPServer();
+	};
+}
